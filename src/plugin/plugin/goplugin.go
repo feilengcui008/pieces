eaf8deade6caefa72193b95de5ce3b5e51c15830
feilengcui008 <feilengcui008@gmail.com>
2017-05-04 13:51:57 +0800

    update

--- src/plugin/plugin/goplugin.go
+++ src/plugin/plugin/goplugin.go
@@ -4,6 +4,7 @@ package main
 
 import "fmt"
 
+//export ExportedForPlugin
 func ExportedForPlugin() {
 	fmt.Println("in ExportedForPlugin func")
 }
--- src/plugin/plugin_main.c
+++ src/plugin/plugin_main.c
@@ -2,10 +2,10 @@
 #include <dlfcn.h>
 
 typedef int (*printf_type)(const char *msg);
+typedef void (*func)();
 
-int main(int argc, char *argv[])
+void test_dlopen_from_c_so() 
 {
-  //const char *path = "/lib/x86_64-linux-gnu/libc.so.6";
   const char *path = "plugin/cplugin.so";
   void *handle = dlopen(path, RTLD_LAZY);
   if (handle == NULL) {
@@ -14,12 +14,11 @@ int main(int argc, char *argv[])
     if (errmsg != NULL) {
       fprintf(stderr, "dlopen errmsg is %s\n", errmsg);
     }
-    return 0;
+    return;
   }
-  //void *fn = dlsym(handle, "printf");
   void *fn = dlsym(handle, "myprintf");
   if (fn == NULL) {
-    fprintf(stderr, "snprintf is not in libc\n");
+    fprintf(stderr, "myprintf is not in libc\n");
   } else {
     ((printf_type)fn)("called by dl symble\n");
   }
@@ -27,5 +26,39 @@ int main(int argc, char *argv[])
   if (ret < 0) {
     fprintf(stderr, "close dlopen handle failed with ret %d\n", ret);
   }
+}
+
+void test_dlopen_from_go_so()
+{
+  const char *path = "plugin/goplugin.so";
+  void *handle = dlopen(path, RTLD_LAZY);
+  if (handle == NULL) {
+    const char *errmsg = dlerror();
+    fprintf(stderr, "dlopen failed\n");
+    if (errmsg != NULL) {
+      fprintf(stderr, "dlopen errmsg is %s\n", errmsg);
+    }
+    return;
+  }
+  // not work for c dlopen of go plugin .so file now
+  void *fn = dlsym(handle, "ExportedForPlugin");
+  //void *fn = dlsym(handle, "runtime.printfloat");
+  //void *fn = dlsym(handle, "plugin/unnamed-e1f58c25fd8588f5627fd829684f7dcd66e42b57.ExportedForPlugin");
+  if (fn == NULL) {
+    fprintf(stderr, "ExportedForPlugin is not in libc\n");
+  } else {
+    ((func)fn)();
+  }
+  int ret = dlclose(handle);
+  if (ret < 0) {
+    fprintf(stderr, "close dlopen handle failed with ret %d\n", ret);
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  test_dlopen_from_c_so();
+  fprintf(stdout, "====================\n");
+  test_dlopen_from_go_so();
   return 0;
 }
