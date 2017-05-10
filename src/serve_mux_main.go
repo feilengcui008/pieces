111ddc7e4f3cad9023c4b98270c879af7dbfaf99
feilengcui008 <feilengcui008@gmail.com>
2017-05-10 20:20:53 +0800

    update

--- src/serve_mux_main.go
+++ src/serve_mux_main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/gorilla/mux"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
@@ -27,25 +27,24 @@ func RegisterHandler(mux *http.ServeMux, pattern string, handler http.Handler) {
 }
 
 func main() {
-	mx := http.NewServeMux()
-	RegisterHandler(mx, "/", &HomeHandler{})
-	RegisterHandler(mx, "/post", http.HandlerFunc(PostHandler))
-	err := http.ListenAndServe(":8888", mx)
-	/*
-			r := mux.NewRouter()
-			r.HandleFunc("/mux1/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-				w.WriteHeader(http.StatusOK)
-				fmt.Fprintf(w, "%s %v\n", "MuxRouterHandler", mux.Vars(r))
-			})
-			f := func(w http.ResponseWriter, r *http.Request) {
-				vars := mux.Vars(r)
-				w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-				w.WriteHeader(http.StatusOK)
-				fmt.Fprintf(w, "got vars %v\n", vars)
-			}
-			r.HandleFunc("/mux2/{key}", f)
-		err := http.ListenAndServe(":8888", r)
-	*/
+	// default mux
+	//mx := http.NewServeMux()
+	//RegisterHandler(mx, "/", &HomeHandler{})
+	//RegisterHandler(mx, "/post", http.HandlerFunc(PostHandler))
+	//err := http.ListenAndServe(":8888", mx)
+	r := mux.NewRouter()
+	r.HandleFunc("/mux1/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "%s %v\n", "MuxRouterHandler", mux.Vars(r))
+	})
+	f := func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, "got vars %v\n", vars)
+	}
+	r.HandleFunc("/mux2/{key}", f)
+	err := http.ListenAndServe(":8888", r)
 	log.Printf("%v\n", err)
 }
