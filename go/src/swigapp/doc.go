3d2c5fda456192dc75675c8d886b7480c583ab5c
feilengcui008 <feilengcui008@gmail.com>
2018-01-09 15:53:53 +0800

    add swig demo

---
+++ go/src/swigapp/doc.go
@@ -0,0 +1,3 @@
+package swigapp
+
+// the swig file name must be the same as this package name
---
+++ go/src/swigapp/swigapp.cpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+void CallSimpleFunc() { std::cout << "in CallSimpleFunc" << std::endl; }
