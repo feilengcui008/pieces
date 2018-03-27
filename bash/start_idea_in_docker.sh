1d7081285baeba61a52e8874572f7d45ad19a205
feilengcui008 <feilengcui008@gmail.com>
2018-03-27 10:30:37 +0800

    move bash to my blog cheatsheet page

--- bash/start_idea_in_docker.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-    -dti \
-    -e DISPLAY=$DISPLAY \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v /home/tan/me/workspace/java:/home/developer/dev \
-    --name idea smilingrob/intellij-ce:jdk7-15.0.4
